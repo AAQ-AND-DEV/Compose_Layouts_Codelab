084f2fb339b9bb3564e2b645bec90e728df48e43
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-04-15 12:05:30 -0700

    * passing argument to navigation composable * replace called on Screen.route, to set arg as textField value * remembers mutableStateOf(TextFieldValue()) in Wrapper composable * accesses arg via navBackStackEntry param of composable() lambda

--- app/src/main/java/com/example/composelayoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/composelayoutscodelab/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.composelayoutscodelab
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -12,14 +13,13 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddBusiness
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -50,12 +50,12 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
 @Composable
 fun LayoutsCodelab() {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
     val navController = rememberNavController()
+    val nameInputValue = remember { mutableStateOf(TextFieldValue()) }
 
     Scaffold(
         topBar = {
@@ -79,19 +79,47 @@ fun LayoutsCodelab() {
         drawerContent = { MyDrawer(myDrawerState = drawerState, myScope = scope) }
     )
     { innerPadding ->
-        NavHost(navController, startDestination = Screen.Greeting.route){
-            composable(Screen.Greeting.route){ Greeting(navController) }
-            composable(Screen.PersonalizedGreeting.route){ NewBodyContent(navController)}
+        NavHost(navController, startDestination = Screen.Greeting.route) {
+            composable(Screen.Greeting.route) { Greeting(navController, nameInputValue) }
+            composable(Screen.PersonalizedGreeting.route) { navBackStackEntry ->
+
+                    navBackStackEntry.arguments?.getString("name")?.let {
+                        Log.d("mainAct", it
+                        )
+                    }
+                                NewBodyContent (
+                                navController,
+                        navBackStackEntry.arguments?.getString("name", "empty name")
+                    )
+
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(navController: NavController, modifier: Modifier = Modifier){
-    Button(onClick = {navController.navigate(Screen.PersonalizedGreeting.route){
-        launchSingleTop = true
-    } }){
-        Text("Navigate to Personalized Greeting")
+fun Greeting(
+    navController: NavController,
+    inputValue: MutableState<TextFieldValue>,
+    modifier: Modifier = Modifier
+) {
+    Column() {
+
+        TextField(inputValue.value, onValueChange = { inputValue.value = it },
+            placeholder = { Text(text = "please enter your name") })
+        Button(onClick = {
+            Log.d("mainAct", inputValue.value.text)
+            navController.navigate(
+                Screen.PersonalizedGreeting.route.replace(
+                    "{name}",
+                    inputValue.value.text
+                )
+            ) {
+                launchSingleTop = true
+            }
+        }) {
+            Text("Navigate to Personalized Greeting")
+        }
     }
 }
 
@@ -105,10 +133,14 @@ fun BodyContent(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun NewBodyContent(navController: NavController, modifier: Modifier = Modifier){
-    Row(modifier){
-        Text(stringResource(R.string.other_composable_greeting, "Name"))
-                Text("This is another TV")
+fun NewBodyContent(navController: NavController, name: String?, modifier: Modifier = Modifier) {
+    Row(modifier) {
+        if (name != null) {
+            Text(stringResource(R.string.other_composable_greeting, name))
+        } else {
+            Text(stringResource(R.string.other_composable_greeting, "nameless one"))
+        }
+        Text("This is another TV")
     }
 }
 
--- app/src/main/java/com/example/composelayoutscodelab/nav/Screen.kt
+++ app/src/main/java/com/example/composelayoutscodelab/nav/Screen.kt
@@ -5,5 +5,5 @@ import com.example.composelayoutscodelab.R
 
 sealed class Screen(val route: String, @StringRes val resId: Int){
     object Greeting: Screen("greeting", R.string.greeting_route)
-    object PersonalizedGreeting: Screen("personGreeting", R.string.personal_greeting_route)
+    object PersonalizedGreeting: Screen("personGreeting/{name}", R.string.personal_greeting_route)
 }
